125349e413c23cb2a4e1db426352cd23d31e646c
Mert Bora İnevi <mertborainevi@gmail.com>
2024-01-04 22:24:05 +0100

    console dir headers

--- authentication.js
+++ authentication.js
@@ -2,6 +2,7 @@ import { verifyKey } from "discord-interactions";
 import env from "./env.js";
 
 export function authenticate(headers, rawBody) {
+    console.dir(headers);
     const signature = headers['x-signature-ed25519'] ?? headers['X-Signature-Ed25519'];
     const timestamp = headers['x-signature-timestamp'] ?? headers['X-Signature-Timestamp'];
 
