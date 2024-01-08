b8d0d5fcd60fe47c72d9483fb5d20c64f9a582b9
Mert Bora İnevi <mertborainevi@gmail.com>
2024-01-08 14:32:22 +0100

    adapt index-polka so it calls the vercel function endpoint

--- index-polka.js
+++ index-polka.js
@@ -1,20 +1,31 @@
 import polka from "polka";
 import bodyParser from "body-parser";
-import { handler } from "./index.mjs";
+import handler from "./api/interactions.js";
 
 polka().use(bodyParser.text({
     type: ["application/json", "text/plain", "text/json"],
-})).post("/", async (req, res) => {
-    const response = await handler(req);
-    console.log(response);
-    if (response == 200 || response == 401) {
-        res.statusCode = response;
-        res.end("Done");
-    } else {
-        if (response.headers) Object.entries(response.headers).forEach(([k, v]) => res.setHeader(k, v));
-        console.log(response.body);
-        res.end(response.body);
+})).post("/", /** @type {(req: import("node:http").IncomingMessage, res: import("node:http").ServerResponse) => Promise<void>} */ async (req, res) => {
+    const myRes = {
+        status(n) {
+            console.log("SET STATUS " + n);
+            res.statusCode = n;
+        },
+        send(str) {
+            console.log("RES END ", str);
+            res.end(str);
+        },
+        json(obj) {
+            console.log(obj);
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(obj));
+        },
+        setHeader(k, v) {
+            res.setHeader(k, v);
+        },
     }
+    req.rawBody = req.body;
+    req.body = JSON.parse(req.body);
+    handler(req, myRes);
 }).listen(3000, err => {
     if (err) throw err;
     else console.log("Server listening on port 3000.");
