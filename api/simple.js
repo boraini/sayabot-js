e02fa75c4c238ee37993a5a2c453b6ab7b761fa8
Mert Bora İnevi <mertborainevi@gmail.com>
2024-01-15 02:06:57 +0100

    add simple handler

---
+++ api/simple.js
@@ -0,0 +1,29 @@
+import { InteractionResponseType } from "discord-interactions";
+import { buffer } from "micro";
+import { authenticate } from "../authentication";
+
+export const config = {
+    runtime: "edge",
+    api: {
+        bodyParser: false,
+    },
+};
+
+/**Vercel function handler
+ * 
+ * @param {import("@vercel/node").VercelRequest} req
+ * @param {import("@vercel/node").VercelResponse} res
+ */
+export default async function handle(req, res) {
+    const rawBody = await buffer(req);
+    console.log(rawBody.toString("utf8"));
+
+    if (!authenticate(req.headers, rawBody)) {
+        console.log("Not Authorized!");
+        res.status(401);
+        res.send("Unauthorized");
+        return;
+    }
+    
+    res.json({ type: InteractionResponseType.PONG });
+}
