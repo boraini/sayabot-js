12d8e89ca7c7609a8ca623d8aa48ebc8c23a45b7
Mert Bora İnevi <mertborainevi@gmail.com>
2024-04-28 19:41:01 +0200

    add replying feature

--- api/manual-message.js
+++ api/manual-message.js
@@ -18,6 +18,10 @@ export default async function handle(req) {
             throw new Error("Target message channel is not in a valid format.");
         }
 
+        if (message.message_reference?.message_id && message.message_reference?.message_id != "" && !(/^[0-9]+$/.test(message.message_reference.message_id))) {
+            throw new Error("Replying to is not in a valid format.");
+        }
+
         if (message.content && message.content.length > 2000) {
             throw new Error("Message is too long or not in valid format.");
         }
@@ -26,7 +30,7 @@ export default async function handle(req) {
             method: "POST",
             ...getJSONResponse(message),
         });
-
+        
         return response;
     } catch (e) {
         return new Response("Message sending failed with exception: " + e.message, { status: 502 });
