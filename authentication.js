1555b937a62f3c859d23f7c02bbfd4c1d06bb3fb
Mert Bora İnevi <mertborainevi@gmail.com>
2024-06-10 02:58:22 +0200

    remove console logs

--- authentication.js
+++ authentication.js
@@ -4,10 +4,6 @@ import env from "./env.js";
 export function authenticate(headers, rawBody) {
     const signature = headers['x-signature-ed25519'] ?? headers['X-Signature-Ed25519'];
     const timestamp = headers['x-signature-timestamp'] ?? headers['X-Signature-Timestamp'];
-
-    console.log(signature);
-    console.log(timestamp);
-    console.log(rawBody);
     
     const isValidRequest = verifyKey(rawBody, signature, timestamp, env.clientPublicKey);
 
